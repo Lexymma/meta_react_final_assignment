ada1150ce7dca96b573640afa5994dffb007e7db
lexymma <hemmalexy@gmail.com>
2023-03-08 22:27:35 +0000

    Added functionality and improved scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -34,6 +34,32 @@ const socials = [
 
 
 const Header = () => {
+ const headerRef = useRef(null);
+
+ useEffect(() => {
+  let prevScrollPos = window.scrollY;
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    const headerElement = headerRef.current;
+    if (!headerElement) {
+    return;
+    }
+    if (prevScrollPos > currentScrollPos) {
+      headerElement.style.transform = "translateY(0)";
+    } else {
+      headerElement.style.transform = "translateY(-200px)";
+    }
+      prevScrollPos = currentScrollPos;
+    };
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    };
+ }, []);
+
+
   const handleClick = (anchor) => () => {
     const id = `${anchor}-section`;
     const element = document.getElementById(id);
@@ -45,15 +71,15 @@ const Header = () => {
     }
   };
 
-  const socialItems = socials.map((social, key) => {
-    return (<a key={key} style={{marginRight: "30px"}} href={social.url}><FontAwesomeIcon className="fa-fw" icon={social.icon} size="2x"/> </a>);
-    // const socialList = <FontAwesomeIcon icon={social.icon} size="2x" />
-    // return (
-    //   <a href={social.url}>{socialList}</a>
-    // );
-    // Note: I need check how react.children.map applies to this implementation.
-  });
-
+  // const socialItems = socials.map((social, key) => {
+  //   return (
+  //     <a key={key} style={{marginRight: "30px"}} 
+  //       href={social.url} target="_blank">
+  //       <FontAwesomeIcon className="fa-fw" icon={social.icon} size="2x"/>
+  //     </a>
+  //   );
+  
+  // });
 
 
   return (
@@ -67,6 +93,7 @@ const Header = () => {
       transitionDuration=".3s"
       transitionTimingFunction="ease-in-out"
       backgroundColor="#18181b"
+      ref={headerRef}
     >
       <Box color="white" maxWidth="1280px" margin="0 auto">
         <HStack
@@ -75,13 +102,25 @@ const Header = () => {
           justifyContent="space-between"
           alignItems="center"
         >
-          <nav>        
-            {socialItems}
+          <nav>
+            <HStack spacing={8}>
+            {socials.map(({icon, url}) => (
+              <a 
+              key={url}
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={icon} size="2x" key={url} />
+              </a>
+            ))}
+            </HStack>        
+            
           </nav>
           <nav>
             <HStack spacing={8}>
-              <a href="/#projects-section" onClick={handleClick}>Projects</a>
-              <a href="/#contactme-section" onClick={handleClick}>Contact Me</a>
+              <a href="/#projects" onClick={handleClick("projects")}>Projects</a>
+              <a href="/#contactme" onClick={handleClick("contactme")}>Contact Me</a>
             </HStack>
           </nav>
         </HStack>
@@ -90,6 +129,3 @@ const Header = () => {
   );
 };
 export default Header;
-
-//  {/* Add social media links based on the `socials` data */}
-// {/* Add links to Projects and Contact me section */}
